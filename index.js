1ab83f2f99c21f5601a9861a878d1151ea10234e
Kouhei Okamoto <beruzebuscotty3000@gmail.com>
2021-07-24 20:18:27 +0900

    JSファイル分岐のテスト前

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 const assessmentButton = document.getElementById('assessment');
 const formArea = document.getElementById('form-area');
+const resultArea = document.getElementById('result-area');
 
 const questions = [
   '何事も中途半端に終わらせることを好かない',
@@ -59,27 +60,85 @@ for (let question_num = 1; question_num <= 20; question_num++) {
 }
 
 assessmentButton.onclick = function() {
+  resultArea.innerText = "";
   let score = [];
   for (let question_num = 1; question_num <= 20; question_num++) {
     const key = `question${question_num}`;
     const value = document.getElementById('question')[key].value;
     score.push(parseInt(value));
   }
+  let communicationScore = {"コントローラー": "", "プロモーター": "", "サポーター": "", "アナライザー": ""};
   const controlerScoreList = score.splice(0, 5);
-  const controlerScore = calculateSum(controlerScoreList);
-  console.log(`コントローラースコア: ${controlerScore}`);
+  communicationScore['コントローラー'] = calculateSum(controlerScoreList);
   const promoterScoreList = score.splice(0, 5);
-  const promoterScore = calculateSum(promoterScoreList);
-  console.log(`プロモータースコア: ${promoterScore}`);
+  communicationScore['プロモーター'] = calculateSum(promoterScoreList);
   const supporterScoreList = score.splice(0, 5);
-  const supporterScore = calculateSum(supporterScoreList);
-  console.log(`サポータースコア: ${supporterScore}`);
+  communicationScore['サポーター'] = calculateSum(supporterScoreList);
   const analyzerScoreList = score.splice(0, 5);
-  const analyzerScore = calculateSum(analyzerScoreList);
-  console.log(`アナライザースコア: ${analyzerScore}`);
+  communicationScore['アナライザー'] = calculateSum(analyzerScoreList);
+  console.log(communicationScore);
+
+  const resultFrame = document.createElement('p');
+  resultFrame.innerText = '診断結果';
+  resultArea.appendChild(resultFrame);
+  const scoreFrame = document.createElement('ul');
+  resultArea.appendChild(scoreFrame);
+  for (let key in communicationScore) {
+    const scoreList = document.createElement('li');
+    scoreList.innerText = `${key}スコア: ${communicationScore[key]}`;
+    scoreFrame.appendChild(scoreList);  
+  }
+  let x = (
+    communicationScore['プロモーター'] +
+    communicationScore['サポーター'] -
+    communicationScore['コントローラー'] -
+    communicationScore['アナライザー']) / 1.44;
+  if (x > 5) {x = 5};
+
+  let y = (
+    communicationScore['プロモーター'] -
+    communicationScore['サポーター'] +
+    communicationScore['コントローラー'] -
+    communicationScore['アナライザー']) / 1.44;
+  if (y > 5) {y = 5};
+
+  console.log(x, y);
+  const canvas=document.getElementById('canvas');
+  const ctx=canvas.getContext('2d');
+  const img = new Image();
+  img.onload = function(){
+    ctx.drawImage(img,0,0, 500, 500);
+  // ctx.fillRect(235,235,30,30);
+    ctx.fillStyle = "#f72da574";
+    ctx.fillRect(235 + 35*x, 235 - 35*y, 30, 30);
+  // 中心(250, 250) ただし、マーカーのサイズ分ズレる。
+  // MIN(60, 60)
+  // MAX(410, 410)
+  // グラフの幅、350
+  // 1マス 35
+  };
+  img.src = 'communication-type.png';
+  
 }
 
 function calculateSum (scoreList) {
   const sum = scoreList.reduce((a, b) => a + b, 0);
   return sum;
 }
+
+// const W = 500;
+// const H = 400
+// // x軸の描画
+// ctx.beginPath();
+// ctx.moveTo(10, H-20);
+// ctx.lineTo(W-10, H-20);
+// ctx.closePath();
+// ctx.stroke()
+// // y軸の描画
+// ctx.beginPath();
+// ctx.moveTo(20, H-10);
+// ctx.lineTo(20, 10);
+// ctx.closePath();
+// ctx.stroke();
+// 
+// ctx.fillRect(100,200,10,10);
